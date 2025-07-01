8dea7e467ccb1a21db81b036f0eeac08dcd6cb29
nicolas <nicolas.souza@moinformatica.com>
2025-06-30 21:26:55 -0300

    JWT ADDED

--- src/enums/category-enum.js
+++
@@ -1,6 +0,0 @@
-export var Categories;
-(function (Categories) {
-    Categories["GELADEIRA"] = "GELADEIRA";
-    Categories["FREEZER"] = "FREEZER";
-})(Categories || (Categories = {}));
-export default Categories;
--- src/routes/pingRoute.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-const routes = express.Router();
-routes.get("/ping", (req, res) => {
-    res.send("pong");
-});
-export default routes;
