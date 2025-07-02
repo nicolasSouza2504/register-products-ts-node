efad6eea879cea97f63634bb77e0e85cf148053b
nicolas <nicolas.souza@moinformatica.com>
2025-07-02 19:45:54 -0300

    a

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ class App {
         this.server.use((req: Request, res: Response, next: NextFunction) => {
 
             res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+            res.setHeader('Access-Control-Allow-Headers', '*');
             res.setHeader('Access-Control-Allow-Methods', '*');
 
             next();
